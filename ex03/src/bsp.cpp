f195f0d0ccc5efe66bee732c7a5b8a34ddb475dc
Shun Usami <usatie@gmail.com>
2023-03-28 15:07:39 +0900

    :sparkles: Add comment to bsp

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -1,42 +1,56 @@
 #include "Point.hpp"
 #include <iostream>
-#define FIXED_EPSILON 0.00390625f
+
+//     b
+//    /
+//   /
+//  O--------> a
+Fixed	cross( Point const a, Point const b ) {
+	Fixed f = ( a.getX() * b.getY() - a.getY() * b.getX() );
+	return ( a.getX() * b.getY() - a.getY() * b.getX() ) ;
+}
+
+//     point
+//    /
+//   /
+//  p1-------->p2
+bool	side( Point const p1, Point const p2, Point const point) {
+	return cross( p2 - p1, point - p1 ) > 0 ;
+}
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
-	// Inside if point is (a | b | c)
-	if ( a == point || b == point || c == point )
-		return true ;
-
+	// 1. pre-validation test with bounding box
 	// Get x,y boundaries
+	// a-------
+	// |......c
+	// |......|
+	// |......|
+	// |_b____|
 	Fixed min_x = Fixed::min(Fixed::min(a.getX(), b.getX()), c.getX()) ;
 	Fixed max_x = Fixed::max(Fixed::max(a.getX(), b.getX()), c.getX()) ;
 	Fixed min_y = Fixed::min(Fixed::min(a.getY(), b.getY()), c.getY()) ;
 	Fixed max_y = Fixed::max(Fixed::max(a.getY(), b.getY()), c.getY()) ;
 
-	// Out of boundaries
-	if ( min_x > point.getX()
-		|| max_x < point.getX()
-		|| min_y > point.getY()
-		|| max_y < point.getY() )
+	// On edge is outside
+	// Vertex is outside
+	if ( min_x >= point.getX()
+		|| max_x <= point.getX()
+		|| min_y >= point.getY()
+		|| max_y <= point.getY() )
 		return false ;
 
-	// Within small boundaries, but not (a | b | c)
-	// Case like this
-	// a.-.b
-	//  |/  
-	// c. .p
-	if ( (max_x - min_x) <= FIXED_EPSILON && (max_y - min_y) <= FIXED_EPSILON )
-		return false ;
-
-	// Paritioning the space
-	Point center = (a + b + c) / 3 ;
-
-	// Avoid infinite loop by comparing w/ center
-	if ( c != center && bsp(a, b, center, point) )
-		return true ;
-	if ( b != center && bsp(a, center, c, point) )
-		return true ;
-	if ( a != center && bsp(center, b, c, point) )
-		return true ;
-	return false ;
+	// 2. validation with cross product
+	// a-------
+	// |......c
+	// |..p...|
+	// |......|
+	// |_b____|
+	//
+	// If p is inside of the triangle,
+	// sign of cross(ab, ap), cross(bc, bp) and cross(ca, cp) must be the same.
+	// s1, s2, s3 is the sign of the each cross product
+	bool s1 = side(a, b, point) ;
+	bool s2 = side(b, c, point) ;
+	bool s3 = side(c, a, point) ;
+	return (s1 || s2 || s3) == (s1 && s2 && s3) ;
 }
--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -20,24 +20,39 @@ void test(std::string info, const Point a, const Point b, const Point c, const P
 
 #define EPS 0.00390625f
 int main(void) {
-    Point p;
-    test("...", Point(), Point(), Point(), p);
-    test("|._\\", Point(0,1), Point(0,0), Point(1,0), p);
-    test("/_._\\", Point(0,1), Point(-1,0), Point(1,0), p);
-    test(".\\|", Point(0,1), Point(1,1), Point(1,0), p);
-    test(".//", Point(-1,-1), Point(1,1), Point(1,0), p);
+	{
+		Point p;
 
-    test("\\./", Point(1,1), Point(0,-1), Point(-1,1), p);
-    test("<.|", Point(1,1), Point(-1,0), Point(1,-1), p);
-    test("|.>", Point(-0.125f,-0.125f), Point(0.125f,-0.125f), Point(0,0.125f), p);
-    test("/./", Point(-4242,-1), Point(0,1), Point(4242,0), p);
+		std::cout << "==================== ZERO TEST ====================" << std::endl ;
+		test("...", Point(), Point(), Point(), p);
+	}
+	{
+		Point p;
 
-    test("|._\\", Point(0,EPS), Point(0,0), Point(EPS,0), p);
-    test("/_._\\", Point(0,EPS), Point(-EPS,0), Point(EPS,0), p);
-    test(".\\|", Point(0,EPS), Point(EPS,EPS), Point(EPS,0), p);
-    test(".//", Point(-EPS,-EPS), Point(EPS,EPS), Point(EPS,0), p);
+		std::cout << "==================== NORMAL TEST ====================" << std::endl ;
+		test("|._\\", Point(0,1), Point(0,0), Point(1,0), p);
+		test("/_._\\", Point(0,1), Point(-1,0), Point(1,0), p);
+		test(".\\|", Point(0,1), Point(1,1), Point(1,0), p);
+		test(".//", Point(-1,-1), Point(1,1), Point(1,0), p);
 
-    test("\\./", Point(EPS,EPS), Point(0,-EPS), Point(-EPS,EPS), p);
-    test("<.|", Point(EPS,EPS), Point(-EPS,0), Point(EPS,-EPS), p);
+		test("\\./", Point(1,1), Point(0,-1), Point(-1,1), p);
+		test("<.|", Point(1,1), Point(-1,0), Point(1,-1), p);
+		test("|.>", Point(-0.125f,-0.125f), Point(0.125f,-0.125f), Point(0,0.125f), p);
+		test("/./", Point(-4242,-1), Point(0,1), Point(4242,0), p);
+	}
+	{
+		Point p;
+
+		std::cout << "==================== EPSILON TEST ====================" << std::endl ;
+		test("|._\\", Point(0,EPS), Point(0,0), Point(EPS,0), p);
+		test("/_._\\", Point(0,EPS), Point(-EPS,0), Point(EPS,0), p);
+		test(".\\|", Point(0,EPS), Point(EPS,EPS), Point(EPS,0), p);
+		test(".//", Point(-EPS,-EPS), Point(EPS,EPS), Point(EPS,0), p);
+
+		test("\\./", Point(EPS,EPS), Point(0,-EPS), Point(-EPS,EPS), p);
+		test("<.|", Point(EPS,EPS), Point(-EPS,0), Point(EPS,-EPS), p);
+		test("|.>", Point(-EPS,-EPS), Point(EPS,-EPS), Point(0,EPS), p);
+		test("/./", Point(-4242,-EPS), Point(0,EPS), Point(4242,0), p);
+	}
     return 0;
 }
