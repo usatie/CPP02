ab481247ea1b40fb53f42e8da47bed60c2ccbc78
Shun Usami <usatie@gmail.com>
2023-03-30 14:21:49 +0900

    Add output to getRawBits and setRawBits

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,17 +2,33 @@
 
 #include "Fixed.hpp"
 
+// print title in green and highlighted
+void printTitle(std::string title) {
+  std::cout << "\033[1;32m" << title << "\033[0m" << std::endl;
+}
+
 int main(void) {
-  Fixed a;
-  Fixed b(a);
-  Fixed c;
-  c = b;
-  std::cout << a.getRawBits() << std::endl;
-  std::cout << b.getRawBits() << std::endl;
-  std::cout << c.getRawBits() << std::endl;
-  a.setRawBits(1);
-  std::cout << a.getRawBits() << std::endl;
-  a.setRawBits(-1);
-  std::cout << a.getRawBits() << std::endl;
+  // tests from subject pdf
+  {
+    printTitle("Tests from subject pdf");
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+  }
+  // tests setRawBits
+  {
+    printTitle("Tests setRawBits");
+    Fixed a;
+
+    a.setRawBits(1);
+    std::cout << a.getRawBits() << std::endl;
+    a.setRawBits(-1);
+    std::cout << a.getRawBits() << std::endl;
+  }
   return 0;
 }
